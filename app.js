b2cea71a3980b6128868d8127f29ded8d57a0dda
fdg <olearamon3@gmail.com>
2024-06-17 13:58:34 -0600

    carga final

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 /* BASE DE DATOS */
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 require('dotenv').config()
 const uri = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@nodejs.hgyhsse.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority&appName=nodejs`;
@@ -22,6 +26,8 @@ app.get("/", (req, res) => {
     descripcion: "descripcion"
   });
 });
+
+
 //Rutas web
 app.use('/', require('./router/rutas'));
 app.use('/mascotas', require('./router/Mascota'));
--- router/Mascota.js
+++ router/Mascota.js
@@ -15,5 +15,86 @@ router.get('/', async (req, res) => {
         console.log(error)
     }
 })
+router.get('/crear', (req, res) => {
+    res.render('crear')
+  })
+  router.post('/', async (req, res) => {
+    const body = req.body
+    console.log(body)
+    try {
+        const mascotaDB = new Mascota(body)
+        await mascotaDB.save()
+        res.redirect('/mascotas')
+    } catch (error) {
+        console.log('error', error)
+    }
+})
+router.get('/:id', async(req, res) => {
+    const id = req.params.id
+    try {
+        const mascotaDB = await Mascota.findOne({ _id: id })
+        console.log(mascotaDB)
+        res.render('detalle', {
+            mascota: mascotaDB,
+            error: false
+        })
+    } catch (error) {
+        console.log('erroooooooooorrr', error)
+        res.render('detalle', {
+            error: true,
+            mensaje: 'No se encuentra el documento...'
+        })
+    }
+})
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log('id desde backend', id)
+    try {
 
+        const mascotaDB = await Mascota.findByIdAndDelete({ _id: id });
+        console.log(mascotaDB)
+
+        // https://stackoverflow.com/questions/27202075/expressjs-res-redirect-not-working-as-expected
+        // res.redirect('/mascotas')
+        if (!mascotaDB) {
+            res.json({
+                estado: false,
+                mensaje: 'No se puede eliminar'
+            })
+        } else {
+            res.json({
+                estado: true,
+                mensaje: 'eliminado!'
+            })
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+
+    console.log(id)
+    console.log('body', body)
+
+    try {
+        const mascotaDB = await Mascota.findByIdAndUpdate(
+            id, body, { useFindAndModify: false }
+        )
+        console.log(mascotaDB)
+        res.json({
+            estado: true,
+            mensaje: 'Mascota editada'
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            estado: false,
+            mensaje: 'Mascota falla'
+        })
+    }
+})
 module.exports = router;
