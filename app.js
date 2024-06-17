a152673fb57240b4ccfa00c4ac3eda67c27cf0c7
fdg <olearamon3@gmail.com>
2024-06-17 10:55:53 -0600

    carga

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+/* BASE DE DATOS */
+const mongoose = require('mongoose');
+
+
+require('dotenv').config()
+const uri = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@nodejs.hgyhsse.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority&appName=nodejs`;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(()=> console.log('conectado a mongodb')) 
+  .catch(e => console.log('error de conexión', e))
+/* FIN BDD */
+// Motor de plantilla
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) => {
+  res.render('index',{
+    titulo : "titulo",
+    descripcion: "descripcion"
+  });
+});
+//Rutas web
+app.use('/', require('./router/rutas'));
+app.use('/mascotas', require('./router/Mascota'));
+app.get("/servicios", (req, res) => {
+  res.render('servicios',{titulo : "servicios"});
+});
+
+app.use((req, res,next) => {
+  res.render('404');
+});
+app.use(express.static(__dirname +'/public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ router/Mascota.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const Mascota = require('../models/mascota')
+
+router.get('/', async (req, res) => {
+    try {
+        const arrayMascotas = await Mascota.find();
+        console.log(arrayMascotas)
+        res.render("mascotas", {
+            listaMascotas: "Aquí irán todas las mascotas",
+            arrayMascotas
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = router;
